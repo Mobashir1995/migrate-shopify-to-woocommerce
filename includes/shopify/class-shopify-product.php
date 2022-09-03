2cf1724c4eff35ed8aecc76c2b1ec6e63c0525f6
mobashir2762 <md.mobashir@selise.ch>
2022-09-04 00:29:15 +0600

    remove unnecessary code and format

--- includes/shopify/class-shopify-product.php
+++ includes/shopify/class-shopify-product.php
@@ -1,127 +1,118 @@
 <?php
 /**
- * 
+ *
  */
 swm_override_php_max_values();
 class SWM_Shopify_Product
 {
-	private static $instance;
+    private static $instance;
 
-	public static function getInstance() {
+    public static function getInstance()
+    {
         if (!isset(self::$instance)) {
             self::$instance = new SWM_Shopify_Product();
         }
         return self::$instance;
     }
-	
-	function __construct() { }
 
-	//get all shopify products
-	public function get_all_shopify_products(){
-		global $swm_woocommerce_product;
-		$result_per_request = get_option('wb_swm_result_per_request') ? get_option('wb_swm_result_per_request') : SWM_DEFAULT_RESULT_PER_REQUEST;
+    public function __construct()
+    {
+    }
 
-		swm_create_cache_directory('products');
+    //get all shopify products
+    public function get_all_shopify_products()
+    {
+        global $swm_woocommerce_product;
+        $result_per_request = get_option('wb_swm_result_per_request') ? get_option('wb_swm_result_per_request') : SWM_DEFAULT_RESULT_PER_REQUEST;
+        swm_create_cache_directory('products');
+        $url = swm_set_store_url_query().'products.json';
+        $total_products = $this->count_total_shopify_product();
+        $pages = ceil($total_products['count'] / $result_per_request);
+        $products_list = array();
+        $insert_as_wc = '';
+        $product_directory_name = swm_get_cache_directory('products');
+        $response = array();
+        $response['total_products'] = $total_products['count'];
+        $response['total_page_num'] = $pages;
+        $import_records_options = array(
+            'imported_page'	=> 0,
+            'currently_imported_page'	=> 1,
+            'total_pages' => $pages,
+            'total_import_product' => 0,
+            'total_shopify_products' => $total_products['count']
+        );
+        $import_records = get_option('swm_import_records', $import_records_options);
+        $url = add_query_arg(array(
+                    'limit' => $result_per_request,
+                ), $url);
+        if (isset($import_records['next']) && !empty($import_records['next'])) {
+            $url = add_query_arg(array(
+                    'page_info' => $import_records['next'],
+                ), $url);
+        }
+        $request = swm_remote_request($url);
+        $result = isset($request['products']) ? $request['products'] : array() ;
+        $add_product_cache = swm_create_cache_files($product_directory_name.'/products-cache-'.$import_records['currently_imported_page'].'.txt', json_encode($result));
+        $insert_as_wc .= $swm_woocommerce_product->import_wc_product($result);
+        $response['imported_products_title'] = $insert_as_wc;
+        $swm_import_records = array(
+            'imported_page'	=> $import_records['imported_page']+1,
+            'currently_imported_page'	=> $import_records['currently_imported_page']+1,
+            'total_pages' => $pages,
+            'total_import_product' => $import_records['total_import_product'] + count($result),
+            'total_shopify_products' => $total_products['count']
+        );
+        if (isset($request['next']) && !empty($request['next'])) {
+            $swm_import_records['next'] = $request['next'];
+        }
+        update_option('swm_import_records', $swm_import_records);
+        $response['swm_import_records'] = get_option('swm_import_records', $import_records_options);
+        return $response;
+    }
 
-		$url = swm_set_store_url_query().'products.json';
-		$total_products = $this->count_total_shopify_product();
-		$pages = ceil( $total_products['count'] / $result_per_request );
+    public function get_product_by_collection_id($collection_id)
+    {
+        $product_ids = array();
+        $url = swm_set_store_url_query() . 'products.json';
+        $url = add_query_arg(array(
+                    'collection_id' => $collection_id,
+                    'limit' => 250,
+                    'fields' => 'id',
+                ), $url);
+        $products_by_collection_id = swm_remote_request($url);
+        $products = isset($products_by_collection_id['products']) ? $products_by_collection_id['products'] : array();
+        foreach ($products as $product) {
+            $product_ids[] = $product['id'];
+        }
+        return $product_ids;
+    }
 
-		$products_list = array();
-		$insert_as_wc = '';
-		$product_directory_name = swm_get_cache_directory('products');
+    //get total count of shopify products
+    public function count_total_shopify_product()
+    {
+        $url = swm_set_store_url_query().'products/count.json';
+        $result = swm_remote_request($url);
 
-		$response = array();
-		$response['total_products'] = $total_products['count'];
-		$response['total_page_num'] = $pages;
+        return $result;
+    }
 
-		$import_records_options = array(
-			'imported_page'	=> 0,
-			'currently_imported_page'	=> 1,
-			'total_pages' => $pages,
-			'total_import_product' => 0,
-			'total_shopify_products' => $total_products['count']
-		);
-		$import_records = get_option('swm_import_records', $import_records_options);
+    public function count_total_product_by_collection_id($collection_id)
+    {
+        $url = swm_set_store_url_query() . 'products/count.json';
+        $url = add_query_arg(array(
+                        'collection_id' => $collection_id,
+                    ), $url);
+        $result = swm_remote_request($url);
+        return $result;
+    }
 
-		$url = add_query_arg(array(
-					'limit' => $result_per_request,
-				), $url);
-
-		if( isset($import_records['next']) && !empty($import_records['next']) ){
-			$url = add_query_arg(array(
-					'page_info' => $import_records['next'],
-				), $url);
-		}
-
-		$request = swm_remote_request( $url );
-		$result = isset( $request['products'] ) ? $request['products'] : array() ;
-		$add_product_cache = swm_create_cache_files( $product_directory_name.'/products-cache-'.$import_records['currently_imported_page'].'.txt', json_encode($result) );
-
-		$insert_as_wc .= $swm_woocommerce_product->import_wc_product($result);
-		$response['imported_products_title'] = $insert_as_wc;
-
-		$swm_import_records = array(
-			'imported_page'	=> $import_records['imported_page']+1,
-			'currently_imported_page'	=> $import_records['currently_imported_page']+1,
-			'total_pages' => $pages,
-			'total_import_product' => $import_records['total_import_product'] + count($result),
-			'total_shopify_products' => $total_products['count']
-		);
-
-		if( isset($request['next']) && !empty($request['next']) ){
-			$swm_import_records['next'] = $request['next'];
-		}
-		
-		update_option('swm_import_records', $swm_import_records);
-
-		$response['swm_import_records'] = get_option('swm_import_records', $import_records_options);
-			
-		return $response;
-		
-	}
-
-	public function get_product_by_collection_id($collection_id){
-		$total_products = $this->count_total_product_by_collection_id($collection_id);
-		$product_ids = array();
-		$pages = ceil( $total_products['count'] / 250 );
-		for ($i=1; $i<=$pages ; $i++) { 
-			$url = swm_set_store_url_query() . 'products.json';
-			$url = add_query_arg(array(
-						'collection_id' => $collection_id,
-						'limit' => 250,
-						'page' => $i,
-						'fields' => 'id',
-					), $url);
-			$products_by_collection_id = swm_remote_request( $url );
-			$products = isset($products_by_collection_id['products']) ? $products_by_collection_id['products'] : array();
-			foreach ($products as $product) {
-				$product_ids[] = $product['id'];
-			}
-		}
-		return $product_ids;
-	}
-
-	//get total count of shopify products
-	public function count_total_shopify_product(){
-		$url = swm_set_store_url_query().'products/count.json';
-		$result = swm_remote_request( $url );
-
-		return $result;
-	}
-
-	public function count_total_product_by_collection_id($collection_id){
-		$url = swm_set_store_url_query() . 'products/count.json';
-		$url = add_query_arg(array(
-						'collection_id' => $collection_id,
-					), $url);
-		$result = swm_remote_request( $url );
-		return $result;
-	}
-
-	// prevent the instance from being cloned
-    private function __clone(){}
+    // prevent the instance from being cloned
+    private function __clone()
+    {
+    }
 
     // prevent from being unserialized
-    private function __wakeup(){}
+    private function __wakeup()
+    {
+    }
 }
